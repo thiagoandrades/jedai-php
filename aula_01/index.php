e25062d3bf8a2b1e9d624d2018c0ff9bd58ed3eb
thiagoandrades <dev@thiagoandrade.me>
2022-04-05 00:02:25 -0300

    first commit

---
+++ aula_01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title>Minha página Teste</title>
+</head>
+<body>
+
+</body>
+</html>>
