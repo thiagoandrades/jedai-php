5c4e7b6525497963144b78178bb95b5107429174
thiagoandrades <dev@thiagoandrade.me>
2022-04-06 09:04:06 -0300

    operadores

---
+++ modulo_05/aula_10/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+	$nome = 'gui';
+	$nome2 = 'joão';
+
+	//um sinal igual quer dizer que estamos atribuindo um valor
+	//sempre retorna verdadeiro
+
+	if($nome == $nome2){
+
+		echo 'sao iguais';
+	}
+
+	if($nome != $nome2){
+
+		echo 'sao diferentes';
+	}
+
+	if($nome === $nome2){
+
+		echo 'sao identicos';
+	}
+
+ ?>
