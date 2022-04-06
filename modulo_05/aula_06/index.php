36da87fec93dc6058c9887e2b09407c501fe335c
thiagoandrades <dev@thiagoandrade.me>
2022-04-06 00:22:19 -0300

    arrays

---
+++ modulo_05/aula_06/index.php
@@ -0,0 +1,16 @@
+<?php
+	$nome = array('Thiago', 'Andrade', 'Silva');
+
+	echo $nome [1];
+
+	$nomes[] = 'Zeh';
+	$nomes[] = 'jaum';
+
+	echo $nomes [1];
+
+	$endereço ['rua'] = 'avenida joao kerhle';
+	$endereço ['cep'] = '56912-060';
+	$endereço ['numero'] = '2507';
+
+?>
+
