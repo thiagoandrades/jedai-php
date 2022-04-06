65b1164ab917211b78703187ed8fc7ac7e85bca2
thiagoandrades <dev@thiagoandrade.me>
2022-04-06 09:31:29 -0300

    loopings

---
+++ modulo_05/aula_12/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+	// laço de repetição for
+	for($contador=0 ; $contador<10 ; $contador++){
+		echo '<br>ola mundo ';
+		echo $contador;
+	} 
+
+	// laço de repetição while
+
+	$contador = 0;
+	while($contador <10){
+		echo 'hello word'.$contador;
+		echo '<hr>';
+		$contador+=2;
+	}
+
+	// laço do
+
+	do {
+		echo 'Ola el Mundo';
+		echo 'br';
+
+	}while($contador<15);
+	
+ ?>
