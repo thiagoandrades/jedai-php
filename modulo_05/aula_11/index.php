a8bb9a8428a605eaaa9332fda27689026a475969
thiagoandrades <dev@thiagoandrade.me>
2022-04-06 09:21:25 -0300

    operadores matemáticos e lógicos

---
+++ modulo_05/aula_11/index.php
@@ -0,0 +1,44 @@
+<?php 
+
+// operadores matemáticos
+	$numero1 = 10;
+	$numero2 = 11;
+
+/*
+	if($numero1 < $numero2){
+
+	}else{
+
+	}
+
+	if($numero1 <= $numero2){
+		echo 'execute';
+	}else{
+
+	}
+
+
+	if($numero1 > $numero2){
+		
+	}else{
+
+	}
+
+	if($numero1 >= $numero2){
+		echo 'execute';
+	}else{
+
+	}
+*/
+
+	$var1 = 'valor';
+	$var2 = 'valor';
+	$var3 = 'valor';
+
+// operadores lógicos || e &&
+	if (($var1 ==$var2) && ($var2 ==$var3) ) {
+		echo 'eh verdade';
+	}
+
+
+ ?>
