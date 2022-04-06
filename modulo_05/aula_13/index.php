e89e8167d4c327cbf65059369dd37e96e84cf997
thiagoandrades <dev@thiagoandrade.me>
2022-04-06 09:33:59 -0300

    forma de fazer comentarios

---
+++ modulo_05/aula_13/index.php
@@ -0,0 +1,14 @@
+<?php 
+	//variavel que declara nome da pessoa
+	$nome = 'Thiago';
+
+	/*
+	agora vou declarar uma varial
+	esta variavel eh mega importante
+	*/
+
+	$idade = 29;
+
+	#outra forma de fazer comentários
+
+ ?>
