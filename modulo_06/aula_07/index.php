fe07bb584e6ebcb02cf24cfe0b7451e416eca264
thiagoandrades <dev@thiagoandrade.me>
2022-04-10 11:36:18 -0300

    switch, continue e break

---
+++ modulo_06/aula_07/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+	$nome = 'Guilherme';
+
+	switch ($nome) {
+		case 'Guilherme':
+			echo 'meu nome é Guilherme';
+			break;
+		case 'Joao':
+			echo 'meu nome é João e não eh guilherme';
+			break;
+	}
+
+
+ ?>
