aa8c0a4cbe8f5671d865eefbb1bf9d34e0882461
thiagoandrades <dev@thiagoandrade.me>
2022-04-10 11:10:31 -0300

    funcoes nativas date e include

---
+++ modulo_06/aula_05/footer.php
@@ -0,0 +1,3 @@
+aqui eh meu footer
+</body>
+</html>
---
+++ modulo_06/aula_05/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+include('header.php');
+
+/*
+
+	//trabalhando com datas - funções nativas do php
+
+	date_default_timezone_set('America/Sao_Paulo');
+
+	$data = date('d/m/Y H:i:s');
+
+	echo $data;
+
+*/
+
+include('footer.php');
+ ?>
