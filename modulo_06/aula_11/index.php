3ad27b714b554c03bece5b2a2ec89304ccbb5cc2
thiagoandrades <dev@thiagoandrade.me>
2022-04-10 15:50:37 -0300

    sintaxe alternativa no php

---
+++ modulo_06/aula_11/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+	//sintaxe alternativa ao php
+	$nome = 'thiago';
+
+	if($nome == 'thiago'):
+		echo 'verdade';
+	endif;
+
+	while ($nome == 'thiago'):
+		echo 'imprimo o WHILE';
+	endwhile;
+ ?>
