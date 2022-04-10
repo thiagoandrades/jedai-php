ef9d3cc6690622038f443f34f81eb2194b64196c
thiagoandrades <dev@thiagoandrade.me>
2022-04-10 10:22:52 -0300

    die e sleep

---
+++ modulo_06/aula_03/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+	
+
+	//mata o código com DIE
+
+	$nome = 'Thiago';
+
+	if ($nome == 'Ze') {
+		echo 'Tudo certo';
+	} else {
+		die('script encerrado');
+	}
+
+
+	//faz o código durma por determinado tempo
+	sleep(3);
+
+	echo 'Hello World';
+
+
+
+ ?>
